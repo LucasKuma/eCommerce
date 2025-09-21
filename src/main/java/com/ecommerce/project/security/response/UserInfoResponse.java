e05e667d0303bde23b04ce2d90e4882f9ae9e2b1
Kuhlmann <lucas.khmn@gmail.com>
2025-09-21 12:37:34 -0300

    Signin Endpoint implemented

--- src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
+++ src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
@@ -1,18 +1,35 @@
-package com.ecommerce.project.security.jwt;
+package com.ecommerce.project.security.response;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.util.List;
 
-public class LoginResponse {
+public class UserInfoResponse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
 
     private String jwtToken;
 
     private String username;
     private List<String> roles;
 
-    public LoginResponse(String jwtToken, String username, List<String> roles) {
+    public UserInfoResponse(Long id, String jwtToken, String username, List<String> roles) {
+        this.id = id;
         this.jwtToken = jwtToken;
         this.username = username;
         this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getJwtToken() {
